f4e7dcee01a5b55923b752f20017b0454eb94c41
gudzan <andreeva.anva@gmail.com>
2024-06-05 18:03:08 +0300

    login

--- src/app/components/layouts/login.jsx
+++ src/app/components/layouts/login.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-    return <h1>Login</h1>;
+    const [login, setLogin] = useState("")
+    function handleClick(e){
+        setLogin(e.target.value)
+        console.log(e.target.value);
+    }
+    return (
+        <form action="">
+            <div className="m-3">
+                <label htmlFor="login">Логин:</label>
+                <input id="login" type="text" name="login" value={login} onChange={handleClick}/>
+            </div>
+            <div className="m-3">
+                <label htmlFor="password">Пароль:</label>
+                <input id="password" type="password" name="password" />
+            </div>
+
+            <button className="m-3 btn btn-primary" type="submit">Сохранить</button>
+        </form>
+    );
 }
